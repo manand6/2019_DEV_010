c8eef8ce9d4d29ea858ecf1a410f06d1bfa628b9
manand6 <m.anand6@tcs.com>
2018-12-12 17:09:19 +0530

    Add style changes for the App component and test if the handle play is succesfully passing value to scoreboard

--- src/Tennis/Tennis.test.js
+++ src/Tennis/Tennis.test.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Tennis from './Tennis';
+import { configure , shallow , mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure ({ adapter: new Adapter() });
+
+describe('Tennis Component', () =>
+{
+
+  let wrapper;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Tennis />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('handle play function called', () => {
+  let props = {
+      enable:true
+    };
+    wrapper = mount(<Tennis {...props} />)
+    wrapper.instance().handlePlay = jest.fn();
+    wrapper.instance().forceUpdate();
+    console.log(wrapper);
+    wrapper.update();
+    wrapper.find('button').simulate('click');
+    expect(wrapper.instance().handlePlay).toHaveBeenCalled();
+    console.log(wrapper.state());
+  });
+
 });
