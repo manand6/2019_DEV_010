51608ee41c9ca2786449a689a701b43e8c5e1642
manand6 <m.anand6@tcs.com>
2018-12-12 16:12:02 +0530

    passing data from parent to child - App to Tennis component

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,61 @@
 import React, { Component } from 'react';
 import './App.css';
+import Tennis from '../Tennis/Tennis';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+        player2Serve:false,
+        enableGame:false,
+    };
+
+    this.handleToss = this.handleToss.bind(this);
+
+
+  }
+
+      //toss button
+      handleToss(){
+
+        let min = 0;
+        let max = 1;
+        var toss1 = Math.floor(Math.random() * (max - min + 1)) + min;
+        let heads = 0;
+        let tails = 1;
+        var tossChosen = Math.floor(Math.random() * (tails - heads + 1)) + heads;
+        toss1 === tossChosen ?
+          this.setState({
+              player2Serve: true,
+              enableGame: true
+          })
+        :
+        this.setState({
+              player2Serve: false,
+              enableGame: true,
+        })
+    }
+
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p> Tennis Kata</p>
-        </header>
+      <div className="container-fluid">
+        <div className="container">
+        <div className="row">
+            <div className="col-md-4">
+                            {<button className={'btn'} onClick={this.handleToss}>Toss</button>}
+                            </div>
+                            <div className="col-md-4">
+                            <p>Game starts</p>
+                            <div className="col-md-4">
+                            {this.state.enableGame ? <Tennis enable={this.state.enableGame}/> : null }
+                            </div>
+                            </div>
+                            </div>
+      </div>
       </div>
     );
   }
